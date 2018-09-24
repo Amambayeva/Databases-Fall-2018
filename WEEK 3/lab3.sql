dd5258983db42f7d27c14e3eb3ec72413bc6780b
miracle1698 <39824103+miracle1698@users.noreply.github.com>
2018-09-24 18:59:48 +0600

    Update lab3.sql

--- WEEK 3/lab3.sql
+++ WEEK 3/lab3.sql
@@ -54,22 +54,26 @@ INSERT INTO customers(name,lastname, city) VALUES('Julia','Adams', 'Astana');
 
 SELECT lastname from employees;
 SELECT DISTINCT lastname from employees;
-SELECT * from employees WHERE lastname='Smith';
-SELECT * from employees WHERE lastname='Smith' OR lastname='Doe';
-SELECT * from employees WHERE department=14;
-SELECT * from employees WHERE department=37 OR department=77;
+SELECT * from employees WHERE lastname = 'Smith';
+SELECT * from employees WHERE lastname = 'Smith' OR lastname = 'Doe';
+SELECT * from employees WHERE department = 14;
+SELECT * from employees WHERE department = 37 OR department = 77;
 SELECT sum(budget) FROM departments;
 SELECT department, count(*) FROM employees GROUP BY(department);
-SELECT department FROM employees GROUP BY(department) HAVING count(*)>2;
+SELECT department FROM employees GROUP BY(department) HAVING count(*) > 2;
 SELECT DISTINCT ON(budget) name FROM departments ORDER BY budget DESC LIMIT 1 OFFSET 1;
-SELECT name, lastname FROM employees WHERE department=( SELECT code FROM departments ORDER BY budget LIMIT 1);
-SELECT name from employees WHERE city='Almaty' UNION
-SELECT name from customers WHERE city='Almaty';
-SELECT * from departments WHERE budget>60000 ORDER BY budget, code DESC;
-UPDATE departments SET budget=budget*0.9 WHERE name =(SELECt name from departments ORDER BY budget LIMIT 1) ;
-UPDATE employees SET department=(SELECT code from departments where name='IT') WHERE department=(SELECT code from departments where name='Research');
+SELECT name, lastname FROM employees WHERE department = ( SELECT code FROM departments ORDER BY budget LIMIT 1);
+SELECT name from employees WHERE city = 'Almaty' 
+UNION SELECT name from customers WHERE city = 'Almaty';
+SELECT * from departments WHERE budget > 60000 ORDER BY budget, code DESC;
+
+UPDATE departments SET budget = budget*0.9 WHERE name = (SELECT name from departments ORDER BY budget LIMIT 1) ;
+UPDATE employees SET department = (SELECT code from departments where name = 'IT') 
+WHERE department = (SELECT code from departments where name = 'Research');
+
 DELETE FROM employees AS e USING departments AS d where d.name='IT' AND e.department=d.code;
 DELETE FROM employees;
+
 SELECT * from employees;
 SELECT * from customers;
 SELECT * from departments;
